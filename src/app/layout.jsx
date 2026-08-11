b0972ec9427b693438f9a313a40f2a2ae2eb7cf8
omoefe <raniem57@gmail.com>
2026-08-11 01:12:14 +0100

    meta pixel

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -80,26 +80,27 @@ export default function RootLayout({ children }) {
         </AuthProvider>
 
         {/* Meta Pixel */}
+
         <Script id="fb-pixel" strategy="afterInteractive">
           {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window,document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1864237134222473');
-            fbq('track', 'PageView');
-          `}
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '27809282782059598');
+    fbq('track', 'PageView');
+  `}
         </Script>
         <noscript>
           <img
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1864237134222473&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=27809282782059598&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
