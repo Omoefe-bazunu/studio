6c6ba8490112f0380c0a26c0e2ab588b3015f3be
omoefe <raniem57@gmail.com>
2026-08-31 19:01:27 +0100

    Hero Button

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -178,7 +178,7 @@ export default function Hero() {
           audience and get them to take action.
         </p>
 
-        <div className="mt-8 flex md:items-center items-start gap-4 flex-col md:flex-row">
+        <div className="mt-8 flex md:items-center  gap-4 flex-col md:flex-row">
           <Button
             size="lg"
             asChild
